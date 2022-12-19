7e29a56f08981df2f4bcf905bd73baa132edcde4
voskanvl <voskanvl@gmail.com>
2022-12-19 10:58:44 +0300

    fixes after review

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import './sass/style.sass';
-import '@splidejs/splide/css';
+// import '@splidejs/splide/css';
 
 // import roller from './roller';
 // import scroll from "./scroll";
-import scrollIO from './scrollIO';
+// import scrollIO from './scrollIO';
 
 /* SLIDES */
 // roller();
 /* SCROLL */
 // scroll();
-scrollIO(document.querySelector<HTMLElement>('.interviews__right'));
-scrollIO(document.querySelector<HTMLElement>('.interviews1__right'));
-scrollIO(document.querySelector<HTMLElement>('.interviews2__right'));
-scrollIO(document.querySelector<HTMLElement>('.approach__left'));
+// scrollIO(document.querySelector<HTMLElement>('.interviews__right'));
+// scrollIO(document.querySelector<HTMLElement>('.interviews1__right'));
+// scrollIO(document.querySelector<HTMLElement>('.interviews2__right'));
+// scrollIO(document.querySelector<HTMLElement>('.approach__left'));
